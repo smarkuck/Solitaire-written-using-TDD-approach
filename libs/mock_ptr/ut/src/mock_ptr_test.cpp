36178d06c04d8a12b3b93a9462c53a24ff34dcc2
Damian Smarkucki <smarkuck@gmail.com>
2021-04-02 19:20:05 +0200

    added mock_ptr interface library for easier testing of mocks created by unique_ptr added build system corrections

---
+++ libs/mock_ptr/ut/src/mock_ptr_test.cpp
@@ -0,0 +1,55 @@
+#include "gmock/gmock.h"
+#include "mock_ptr.h"
+
+using namespace testing;
+
+class DestructionTestMock {
+public:
+    DestructionTestMock() {
+        EXPECT_CALL(*this, destroy());
+    }
+
+    ~DestructionTestMock() {
+        destroy();
+    }
+
+    MOCK_METHOD(void, destroy, (), ());
+};
+
+class mock_ptr_destructionTest: public Test {
+public:
+    mock_ptr<DestructionTestMock> mockPtr;
+};
+
+TEST_F(mock_ptr_destructionTest, deleteRawPointerIfUniquePointerNotCreated) {}
+
+TEST_F(mock_ptr_destructionTest, deleteRawPointerOnlyOnceIfUniquePointerCreated) {
+    mockPtr.make_unique();
+}
+
+TEST_F(mock_ptr_destructionTest, throwExceptionOnCreationOfSecondUniquePointer) {
+    mockPtr.make_unique();
+    EXPECT_THROW(mockPtr.make_unique(), std::runtime_error);
+}
+
+class ExecutionTestMock {
+public:
+    ExecutionTestMock() {
+        EXPECT_CALL(*this, execute());
+    }
+
+    MOCK_METHOD(void, execute, (), ());
+};
+
+class mock_ptr_executionTest: public Test {
+public:
+    mock_ptr<ExecutionTestMock> mockPtr;
+};
+
+TEST_F(mock_ptr_executionTest, asteriskOperatorShouldReturnReferenceToUnderlyingObject) {
+    (*mockPtr).execute();
+}
+
+TEST_F(mock_ptr_executionTest, uniquePointerShouldContainRawPointerFromMockPtr) {
+    mockPtr.make_unique()->execute();
+}
